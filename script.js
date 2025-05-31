f4761bb270021ac8eff711cf7654b2998ebc2996
SpeedyAli <mohdak824@gmail.com>
2025-06-01 01:21:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
+<script async defer src="https://apis.google.com/js/api.js" onload="onApiLoad()"></script>
+<script async defer src="https://accounts.google.com/gsi/client" onload="gisLoaded()"></script>
+
 const developerKey = window.GOOGLE_API_KEY;
 const clientId = window.GOOGLE_CLIENT_ID;
 let oauthToken;
 
 function onApiLoad() {
-  gapi.load('auth', { 'callback': onAuthApiLoad });
+  gapi.load('client:auth2', initAuth);
   gapi.load('picker');
 }
 
-function onAuthApiLoad() {
-  gapi.auth.authorize(
-    {
-      client_id: clientId,
-      scope: ['https://www.googleapis.com/auth/drive.file'],
-      immediate: false
-    },
-    handleAuthResult
-  );
+function initAuth() {
+  gapi.client.init({
+    clientId: clientId,
+    scope: 'https://www.googleapis.com/auth/drive.file'
+  }).then(() => {
+    const authInstance = gapi.auth2.getAuthInstance();
+    if (!authInstance.isSignedIn.get()) {
+      authInstance.signIn().then(() => {
+        oauthToken = authInstance.currentUser.get().getAuthResponse().access_token;
+        createPicker();
+      });
+    } else {
+      oauthToken = authInstance.currentUser.get().getAuthResponse().access_token;
+      createPicker();
+    }
+  });
 }
 
 function handleAuthResult(authResult) {
@@ -68,16 +78,17 @@ function pickerCallback(data) {
     const fileId = data.docs[0].id;
     console.log("Selected File ID:", fileId);
 
-    fetch('https://script.google.com/macros/s/AKfycbyu-RP0tFGtCnuwIRH6fl3oZmXVn5G-VNBS1BlObIiH1iBPfIq_YUiOELFFpH_5GM4WPA/exec', {
-      method: 'POST',
-      body: JSON.stringify({
-        fileId,
-        accessToken
-      }),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
+  // Inside pickerCallback
+fetch('https://script.google.com/macros/s/AKfycbyu-RP0tFGtCnuwIRH6fl3oZmXVn5G-VNBS1BlObIiH1iBPfIq_YUiOELFFpH_5GM4WPA/exec', {
+  method: 'POST',
+  body: JSON.stringify({
+    fileId,
+    accessToken: oauthToken  // use oauthToken, not accessToken
+  }),
+  headers: {
+    'Content-Type': 'application/json'
+  }
+})
     .then(res => res.text())
     .then(response => {
       console.log('Response from Apps Script:', response);
